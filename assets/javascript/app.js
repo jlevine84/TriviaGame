9aec8f1f5ea0fa92418ec7de378d7d38330e994b
Jeff Levine <jlevine_84@yahoo.com>
2019-01-07 17:24:36 -0500

    layout and pseudocoding updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+//Initial loading with instructions and start option
+//Countdown timer for each question
+//1 Question with multiple responses each round
+//Each round displays if answer was correct and if not, displays correct response
+//After all questions are asked, display trivia data output.
