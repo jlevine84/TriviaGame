eb4a3334937a9b8455aa55b708b5f9dd46d4f570
Jeff Levine <jlevine_84@yahoo.com>
2019-01-10 18:22:20 -0500

    objects and js updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,45 +10,51 @@ var game = {
         question: "What character jumped over barrels in Donkey Kong?",
         answer: "Mario",
         funfact: "Mr. Video and Jumpman were the original names for Mario.",
-        options: [],
+        options: ["Mario", "Luigi", "Diddy Kong", "Jump Fox"],
     },
     question3: {
         question: "What was the world's first Arcade Game?",
         answer: "Computer Space", 
         funfact: "Computer Space was derived from an earlier video game called, Spacewar!",
-        options: [],
+        options: ["Computer Space", "Pong", "Joust", "Battlezone"],
     },
     question4: {
         question: "What are the names of the four ghosts in Pac-Man?",
         answer: "Blinky, Pinky, Inky, and Clyde", 
         funfact: "Blinky and Pinky chase Pac-Man directly, Inky uses strategic ambush tactics, and Clyde behaves completely randomly.",
-        options: [],
+        options: ["Blinky, Pinky, Inky, and Clyde", "Simon, Pinky, Clyde, and Ghosty", "Clyde, Simon, Ghosty, and Blinky", "Pinky, Winky, Dinky, and Rhyme"],
     },
     question5: {
         question: "The Super Mushrooms which cause Mario to grow were inspired by what classic story tale?",
         answer: "Alice in Wonderland", 
         funfact: "The mushrooms in Alice in Wonderland caused her to grow!",
-        options: [],
+        options: ["Alice in Wonderland", "Hansel and Gretel", "Jack and the Magic Beanstalk", "The Three Little Pigs"],
     },
     question6: {
         question: "What game caused the current rating system for video games?",
         answer: "Mortal Kombat",
         funfact: "The original Mortal Kombat was developed in 10 months by a group of only four people!",
-        options: [],
+        options: ["Mortal Kombat", "Tekken", "Street Fighter", "Streets of Rage"],
     },
     question7: {
         question: "What company did video game pioneers Nolan Bushnell and Ted Dabney create?",
-        funfact: "Atari's first commercial success was the class game Pong!",
         answer: "Atari",
-        options: [],
+        funfact: "Atari's first commercial success was the class game Pong!",
+        options: ["Atari", "Sega", "Nintendo", "Sony"],
     },
     question8: {
         question: "What is the longest lived game series whose Main Playable Character is a female?",
         answer: "Metroid",
         funfact: "While Samus Aran may not be the first playable female character, Nintendo has been making Metroid games since 1986!", 
-        options: [],
+        options: ["Metroid", "Mortal Kombat", "Pong", "Star Fox"],
     }
 }
+
+//Global Variables
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var timeLeft = 30;
 
 //Function to randomize answer array on propagation 
 function shuffle(array) {
@@ -67,12 +73,9 @@ function shuffle(array) {
         array[randomIndex] = temporaryValue;
     }
     return array;
-}
+};
 
-var timeLeft = 30;
-var elem = $("#timer");
-
-
+ //Countdown timer for each question
 function countdown() {
     if (timeLeft == -1) {
         clearTimeout(0);
@@ -80,12 +83,26 @@ function countdown() {
         $("#timer").text("Countdown: " + timeLeft + " seconds remaining")
         timeLeft--;
     }
+};
+
+function resultCorrect() {
+    $("#funfact").text("Correct!")
+    $("#option0").text(game.question1.funfact)
+    $("#option1").text("")
+    $("#option2").text("")
+    $("#option3").text("")
 }
 
-//Global Variables
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
+function resultIncorrect() {
+    $("#funfact").text("Incorrect! The correct answer is: " + game.question1.answer)
+    $("#option0").text(game.question1.funfact)
+    $("#option1").text("")
+    $("#option2").text("")
+    $("#option3").text("")
+}
+
+//TODO: After all questions are asked, display trivia data output, and have restart/reset button.
+
 
 //Initial loading with instructions and start option
 $(document).ready(function() {
@@ -112,16 +129,23 @@ $(document).ready(function() {
             $("#option" + i).text(game.question1.options[i])
         }
         
+        //Checks an answer  //Each round displays if answer was correct and if not, displays correct response
+        $(".choice").on("click", function() {
+            var answer = $(this).text();
+            if (answer === game.question1.answer) {
+
+                //correct response - add stop to timer and go to response "page"
+                correct++;
+                resultCorrect();
+            } else if (answer !== game.question1.answer) {
+
+                //incorrect response - add stop to timer and go to response "page"
+                incorrect++;
+                resultIncorrect();
+            }
+        });
+
     });
 
-
- //TODO:Countdown timer for each question
-
-
- //1 Question with multiple responses each round
-
- //Each round displays if answer was correct and if not, displays correct response
-
- //TODO: After all questions are asked, display trivia data output, and have restart/reset button.
 
 });
